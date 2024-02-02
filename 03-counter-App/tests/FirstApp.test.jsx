ed23e41036dd35272e2c25ecc92d0357efb7fedc
Priscilla Rojas <73943767+Priscilla-Rojas@users.noreply.github.com>
2024-02-02 09:39:57 -0300

    Pruebas en el renderizado del componente

--- 03-counter-App/tests/FirstApp.test.jsx
+++ 03-counter-App/tests/FirstApp.test.jsx
@@ -3,9 +3,23 @@ import FirstApp from "../src/firstApp"
 
 describe('Pruebas en <FirstApp/>', () => {
   
-  test('Debe Hacer match con el Snapshot', () => { 
-    const title= 'I am title'
-    render( <FirstApp title={title}/> )
+  // test('Debe Hacer match con el Snapshot', () => { 
+  //   const title= 'I am title'
+  //   const { container } =  render( <FirstApp title={title}/> )
     
+  //   expect ( container ).toMatchSnapshot();
+  // })
+  
+  test('debe mostrar el titulo en un H1', () => {
+    const title= 'I am title';
+    const { container, getByText } =  render( <FirstApp title={title}/> )
+    expect( getByText( title )).toBeTruthy(); // verifico que exista el texto 'I am title'
+    // const h1 = container.querySelector('h1');
+    // expect( h1.innerHTML).toContain( title );
+
+
+
+
+
   })
 })
